d837c8c110a30d47d2442b70ebb65a72da6cdc32
Sejin Kim <froydroyce@gmail.com>
2019-06-23 15:48:11 -0600

    Crate Heroku

---
+++ test/features/user_sees_a_homepage_test.rb
@@ -0,0 +1,16 @@
+require './test/test_helper'
+
+class HomepageTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/'
+
+    assert page.has_content?("Welcome!")
+    assert_equal 200, page.status_code
+  end
+
+  def test_page_has_404
+    visit '/hello'
+
+    assert_equal 404, page.status_code
+  end
+end
